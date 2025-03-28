51d899ecb71ed759fd6d8caad2efd3fc2ff2c733
frodkata <frodkata@gmail.com>
2025-03-28 13:53:39 +0200

    Vercel config

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 
-const addonInterface = require("./addon");
+const addonInterface = require("../addon");
 serveHTTP(addonInterface, { port: 7000 });
